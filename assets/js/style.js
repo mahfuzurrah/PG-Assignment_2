8d10bdede6a05a80c3a73be08e7cd89c0fc0811b
mahfuzurrah <mahfuzuruiux@gmail.com>
2024-01-16 22:44:16 +0600

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,38 @@
+/*======= Video ========*/
+
+
+
+/*======= Selector ========*/
+
+function toggleOptions(id) {
+  var options = document.getElementById("selectOptions" + id);
+  if (options.classList.contains("open")) {
+    options.classList.remove("open");
+    options.classList.add("closed");
+  } else {
+    options.classList.remove("closed");
+    options.classList.add("open");
+  }
+}
+
+function selectOption(id, option) {
+  document.getElementById("selectedOption" + id).innerText = option;
+  var options = document.getElementById("selectOptions" + id);
+  options.classList.remove("open");
+  options.classList.add("closed");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  var dropdowns = document.getElementsByClassName("select-options");
+  for (var i = 0; i < dropdowns.length; i++) {
+    var options = dropdowns[i];
+    if (
+      options.classList.contains("open") &&
+      !event.target.matches(".select-box")
+    ) {
+      options.classList.remove("open");
+      options.classList.add("closed");
+    }
+  }
+};
